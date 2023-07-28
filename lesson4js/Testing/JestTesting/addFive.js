ab9537072aca93c3a086477a30965c41ef32cd75
Emre Aygener <emre.aygener@gmail.com>
2023-07-28 18:41:08 +0300

    JavascriptCourseCompleted

---
+++ lesson4js/Testing/JestTesting/addFive.js
@@ -0,0 +1,11 @@
+function addFive(val) {
+    return val + 5;
+};
+
+module.exports = addFive;
+
+/*
+    node --version
+    npm --version
+    jest --version
+*/ 
---
+++ lesson4js/Testing/ProgAssignJestTest/jest-testing/timesTwo.js
@@ -0,0 +1,6 @@
+// Task 1: Code the timesTwo function declaration
+function timesTwo(val) {
+    return 2 * val;
+}
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
